bc51b75f0abb0d374be821d6ad7aeb0b6fbef787
Delvaaa2233 <fadhelmuhammad204@gmail.com>
2026-07-04 07:08:23 +0700

    Update data_perpus.sql

--- data_perpus.sql
+++ data_perpus.sql
@@ -1,43 +1,32 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 23 Jun 2020 pada 06.17
--- Versi server: 10.1.37-MariaDB
--- Versi PHP: 7.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+ /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ /*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `data_perpus`
 --
 
+CREATE DATABASE IF NOT EXISTS `data_perpus`;
+USE `data_perpus`;
+
 -- --------------------------------------------------------
-
---
 -- Struktur dari tabel `log_pinjam`
---
-
+-- --------------------------------------------------------
 CREATE TABLE `log_pinjam` (
-  `id_log` int(11) NOT NULL,
+  `id_log` int(11) NOT NULL AUTO_INCREMENT,
   `id_buku` varchar(10) NOT NULL,
   `id_anggota` varchar(10) NOT NULL,
-  `tgl_pinjam` date NOT NULL
+  `tgl_pinjam` date NOT NULL,
+  PRIMARY KEY (`id_log`),
+  KEY `id_anggota` (`id_anggota`),
+  KEY `id_buku` (`id_buku`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `log_pinjam`
---
 
 INSERT INTO `log_pinjam` (`id_log`, `id_buku`, `id_anggota`, `tgl_pinjam`) VALUES
 (1, 'B001', 'A001', '2020-06-23'),
@@ -46,24 +35,18 @@ INSERT INTO `log_pinjam` (`id_log`, `id_buku`, `id_anggota`, `tgl_pinjam`) VALUE
 (4, 'B002', 'A005', '2020-06-23');
 
 -- --------------------------------------------------------
-
---
 -- Struktur dari tabel `tb_anggota`
---
-
+-- --------------------------------------------------------
 CREATE TABLE `tb_anggota` (
   `id_anggota` varchar(10) NOT NULL,
   `nama` varchar(20) NOT NULL,
   `jekel` enum('Laki-laki','Perempuan') NOT NULL,
   `kelas` varchar(50) NOT NULL,
-  `no_hp` varchar(15) NOT NULL
+  `no_hp` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_anggota`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tb_anggota`
---
-
-INSERT INTO `tb_anggota` (`id_anggota`, `nama`, `jekel`, `kelas`, `no_hp`) VALUES
+INSERT INTO `tb_anggota` VALUES
 ('A001', 'Ana', 'Perempuan', 'juwana', '089987789000'),
 ('A002', 'Bagus', 'Laki-laki', 'demak', '089987789098'),
 ('A003', 'Citra', 'Perempuan', 'demak', '085878526048'),
@@ -71,11 +54,8 @@ INSERT INTO `tb_anggota` (`id_anggota`, `nama`, `jekel`, `kelas`, `no_hp`) VALUE
 ('A005', 'Edi', 'Laki-laki', 'demak', '089987789098');
 
 -- --------------------------------------------------------
-
---
 -- Struktur dari tabel `tb_buku`
---
-
+-- --------------------------------------------------------
 CREATE TABLE `tb_buku` (
   `id_buku` varchar(10) NOT NULL,
   `judul_buku` varchar(30) NOT NULL,
@@ -83,140 +63,62 @@ CREATE TABLE `tb_buku` (
   `pengarang` varchar(30) NOT NULL,
   `penerbit` varchar(30) NOT NULL,
   `th_terbit` year(4) NOT NULL,
-  `jumlah_buku` int(11) DEFAULT NOT NULL
+  `jumlah_buku` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id_buku`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tb_buku`
---
-
-INSERT INTO `tb_buku` (`id_buku`, `judul_buku`, `kategori`, `pengarang`, `penerbit`, `th_terbit`, `jumlah_buku`) VALUES
-('B001', 'Matematika', 'anastasya', 'armi print', 2010),
-('B002', 'RPL 2', 'Eko', 'UMK', 2020),
-('B003', 'C++', 'Anton', 'Toni Perc', 2010),
-('B004', 'CI 4', 'anastasya', 'armi print', 2009),
-('B005', 'Data Mining', 'Anton', 'Toni Perc', 2020);
+INSERT INTO `tb_buku` VALUES
+('B001', 'Matematika', 'Pelajaran', 'Anastasya', 'Armi Print', 2010, 10),
+('B002', 'RPL 2', 'Teknik', 'Eko', 'UMK', 2020, 5),
+('B003', 'C++', 'Pemrograman', 'Anton', 'Toni Perc', 2010, 7),
+('B004', 'CI 4', 'Framework', 'Anastasya', 'Armi Print', 2009, 3),
+('B005', 'Data Mining', 'Komputer', 'Anton', 'Toni Perc', 2020, 4);
 
 -- --------------------------------------------------------
-
---
 -- Struktur dari tabel `tb_pengguna`
---
-
+-- --------------------------------------------------------
 CREATE TABLE `tb_pengguna` (
-  `id_pengguna` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL AUTO_INCREMENT,
   `nama_pengguna` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(35) NOT NULL,
-  `level` enum('Administrator','Petugas','','') NOT NULL
+  `level` enum('Administrator','Petugas') NOT NULL,
+  PRIMARY KEY (`id_pengguna`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tb_pengguna`
---
-
-INSERT INTO `tb_pengguna` (`id_pengguna`, `nama_pengguna`, `username`, `password`, `level`) VALUES
-(1, 'M ivan S', 'admin',md5('123'), 'Administrator'),
-(5, 'Mivan', 'ivan','123', 'Administrator');
+INSERT INTO `tb_pengguna` VALUES
+(1, 'Fadhel Muhammad', 'admin', md5('123'), 'Administrator');
 
 -- --------------------------------------------------------
-
---
 -- Struktur dari tabel `tb_sirkulasi`
---
-
+-- --------------------------------------------------------
 CREATE TABLE `tb_sirkulasi` (
   `id_sk` varchar(20) NOT NULL,
   `id_buku` varchar(10) NOT NULL,
   `id_anggota` varchar(10) NOT NULL,
   `tgl_pinjam` date NOT NULL,
   `tgl_kembali` date NOT NULL,
-  `status` enum('PIN','KEM') NOT NULL
+  `status` enum('PIN','KEM') NOT NULL,
+  PRIMARY KEY (`id_sk`),
+  KEY `id_buku` (`id_buku`),
+  KEY `id_anggota` (`id_anggota`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `tb_sirkulasi`
---
-
-INSERT INTO `tb_sirkulasi` (`id_sk`, `id_buku`, `id_anggota`, `tgl_pinjam`, `tgl_kembali`, `status`) VALUES
+INSERT INTO `tb_sirkulasi` VALUES
 ('S001', 'B001', 'A001', '2020-06-23', '2020-06-30', 'KEM'),
 ('S002', 'B002', 'A001', '2020-06-13', '2020-06-20', 'PIN'),
 ('S003', 'B003', 'A002', '2020-06-22', '2020-06-29', 'PIN'),
 ('S004', 'B002', 'A005', '2020-06-23', '2020-06-30', 'PIN');
 
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `log_pinjam`
---
-ALTER TABLE `log_pinjam`
-  ADD PRIMARY KEY (`id_log`),
-  ADD KEY `id_anggota` (`id_anggota`),
-  ADD KEY `id_buku` (`id_buku`);
-
---
--- Indeks untuk tabel `tb_anggota`
---
-ALTER TABLE `tb_anggota`
-  ADD PRIMARY KEY (`id_anggota`);
-
---
--- Indeks untuk tabel `tb_buku`
---
-ALTER TABLE `tb_buku`
-  ADD PRIMARY KEY (`id_buku`);
-
---
--- Indeks untuk tabel `tb_pengguna`
---
-ALTER TABLE `tb_pengguna`
-  ADD PRIMARY KEY (`id_pengguna`);
-
---
--- Indeks untuk tabel `tb_sirkulasi`
---
-ALTER TABLE `tb_sirkulasi`
-  ADD PRIMARY KEY (`id_sk`),
-  ADD KEY `id_buku` (`id_buku`),
-  ADD KEY `id_anggota` (`id_anggota`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `log_pinjam`
---
-ALTER TABLE `log_pinjam`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `tb_pengguna`
---
-ALTER TABLE `tb_pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `log_pinjam`
---
+-- --------------------------------------------------------
+-- Foreign Key Constraints
+-- --------------------------------------------------------
 ALTER TABLE `log_pinjam`
   ADD CONSTRAINT `log_pinjam_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `tb_anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `log_pinjam_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `tb_buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Ketidakleluasaan untuk tabel `tb_sirkulasi`
---
 ALTER TABLE `tb_sirkulasi`
   ADD CONSTRAINT `tb_sirkulasi_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `tb_buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `tb_sirkulasi_ibfk_2` FOREIGN KEY (`id_anggota`) REFERENCES `tb_anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
